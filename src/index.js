ae86bf67159bc61eef57871f4978b11f50788a3a
Mantik0re <elzakv18@gmail.com>
2023-08-23 09:27:48 +0300

    refactor: add function towelSort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    const currentValue = matrix[i];
+    if (Array.isArray(currentValue)) {
+      result = result.concat(towelSort(currentValue));
+    } else {
+      result.push(currentValue);
+    }
+  }
+  return result;
 }
